ee0696ae0c9084612a194b0ac126d93fcb7e82c7
Hasan Safari <hasan5afari@yahoo.com>
2026-02-05 15:42:21 +0330

    add Triangle lindenmayer system

--- src/main.py
+++ src/main.py
@@ -3,13 +3,12 @@ from lindenmayer_system import LindenmayerSystem
 
 
 def main() -> None:
-    WINDOW_SIZE = (800, 800)
+    WINDOW_SIZE = (1200, 800)
 
-    system, dtheta = LindenmayerSystem.from_json(".\\lindenmayer_systems\\dragon_curve.json")
-    visualizer = Visualizer(WINDOW_SIZE, (250, 250), 20, 0.9, dtheta)
+    system, dtheta = LindenmayerSystem.from_json(".\\lindenmayer_systems\\triangle.json")
+    visualizer = Visualizer(WINDOW_SIZE, (600, 400), 250, 0.65, dtheta)
 
     visualizer.run(system)
-
 
 
 if __name__ == "__main__":
